27572e57b9e8deb85c618854bdf5b847b3bf82aa
Niladri <niladrisengit12@gmail.com>
2023-07-16 16:33:34 +0530

    day-02 authentication

--- server/app.js
+++ server/app.js
@@ -1,15 +1,27 @@
 import express from "express"
 const app = express()
 import dotenv from "dotenv"
-dotenv.config()
+import morgan from "morgan"
+import { connectDB } from "./database/connection.js";
+import  authRouter  from "./routers/authRoutes.js";
+
+
 
 const PORT = process.env.PORT;
 const MODE = process.env.MODE;
 
-app.get("/",(req,res)=>{
-    res.send("Hi I am Backend")
-})
+//database connection
+connectDB()
 
+//middlewares
+app.use(express.json())
+app.use(morgan("dev"))
+
+//defning routes
+app.use("/auth",authRouter)
+
+
+//listening to port
 app.listen(PORT,()=>{
     console.log(`Server is running on ${MODE} mode and on port ${PORT}`)
 })
